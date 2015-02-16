c036c069979cd459a9f5723116ce4d821b70934c
Arved Baus <arved.baus@gmail.com>
2015-02-16 20:03:48 +0100

    Added Setup File

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='IntelligentReader',
+    version='',
+    packages=['src', 'src.View', 'src.Model', 'src.Rules'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
